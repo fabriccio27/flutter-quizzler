81e6411e344267542922a5d16911d7a13b3f3677
fabriccio27 <fabricio_inc27@hotmail.com>
2024-01-30 11:39:19 -0300

    Refactor to use question class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,15 +27,10 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List <Icon> scoreKeeper = [];
-  List <String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-  List <bool> answers = [
-    false,
-    true,
-    true,
+  List <Question> questions = [
+    Question(q:'You can lead a cow down stairs but not up stairs.', a:false),
+    Question(q:'Approximately one quarter of human bones are in the feet.', a:true),
+    Question(q:'A slug\'s blood is green.', a:true),
   ];
 
   int questionNumber = 0;
@@ -45,8 +41,8 @@ class _QuizPageState extends State<QuizPage> {
     });
   }
   void checkAnswer(bool userPickedAnswer) {
-    bool correctAnswer = answers[questionNumber];
-    if (userPickedAnswer == correctAnswer) {
+    Question currentQuestion = questions[questionNumber];
+    if (userPickedAnswer == currentQuestion.questionAnswer) {
       scoreKeeper.add(Icon(
         Icons.check,
         color: Colors.green,
@@ -71,7 +67,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
